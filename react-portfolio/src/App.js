72a2b7569a1e5286ddd114dc959e76f6c1e689a9
unknown <singh.avvi98@gmail.com>
2021-03-13 18:30:08 +1100

    Basic layout in progress

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import dp from "./dp.jpg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="Nav">
+        <ul className="Nav-list">
+          <a className="Nav-item">Home</a>
+          <a className="Nav-item">Projects</a>
+          <a className="Nav-item">Contact Me</a>
+        </ul>
+      </nav>
+      <h1>Avvi Singh</h1>
+      <h2>Honour The Work, and The Work Will Honour You</h2>
+      <div className="Bio-block">
+        <img src={dp} alt="a photo of me" className="bio-img" />
+        <div className="About-me">
+          <h3>A Bit About Me</h3>
+          <p>
+            This is where I will talk about myself. Points to mention include my
+            education, my journey to coding and my future aspirations.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
